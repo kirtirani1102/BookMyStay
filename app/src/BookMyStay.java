fe48bc9c29a1702c53971f22b5bec40237b99829
kirtirani1102 <kirtirani1970@gmail.com>
2026-03-17 14:59:31 +0530

    First commit

---
+++ app/src/BookMyStay.java
@@ -0,0 +1,26 @@
+/**
+ * Use Case 1: Application Entry Point
+ *
+ * This class demonstrates the starting point of the
+ * Hotel Booking Management System.
+ * It prints a welcome message when the program starts.
+ *
+ * @author Kirti
+ * @version 1.0
+ */
+public class BookMyStay {
+
+    /**
+     * Application entry point.
+     *
+     * This method is the first method executed
+     * when the program is launched by the JVM.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
